b2e98a18bd9ced8d5262eb778db4e72887ab4aa6
fecolucci <feh_colucci@hotmail.com>
2025-04-13 16:28:26 -0300

    Inclusão do movimento do cavalo - nivel 2

--- xadrez.c
+++ xadrez.c
@@ -2,30 +2,57 @@
 
 int main () {
     // Mover a Torre 5 casas para a direita
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n"); //Iprime a direção do movimento e quantas casas
+
+    printf("Torre\n");
+    for (int t = 0; t < 5; t++) {
+        printf("Direita\n"); //Iprime a direção do movimento e quantas casas        
     }
+
+    printf("\n");
 
 
     // Mover o Bispo 5 casas na diagonal para cima e a direita
 
-    int i = 0;
+    int b = 0;
 
-    while (i < 5)
+    printf("Bispo\n");
+    while (b < 5)
     {
         printf("Cima -");
         printf(" Direita\n");
-        i++;
+        b++;
     }
+
+    printf("\n");
 
     // Mover a Rainha 8 casas para a esquerda
 
-    int x = 0;
+    int r = 0;
 
+    printf("Rainha\n");
     do {
         printf("Esquerda\n");
-        x++;
-    } while (x < 8);
+        r++;
+    } while (r < 8);
+
+    printf("\n");
+
+    // Mover o Cavalo duas casas para baixo e uma casa para a esquerda
+
+    int a = 0;
+
+    printf("Cavalo\n");
+    for (int c = 0; c < 1; c++)
+    {
+        while (a < 2)
+        {
+            printf("baixo\n");
+            a++;
+        }
+        printf("Esquerda");
+        printf("\n");
+    }
+    
     
     return 0;
 }
