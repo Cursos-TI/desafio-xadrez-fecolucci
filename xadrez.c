a32b7c155ff90935a95268d8b7723fc977ac23be
fecolucci <feh_colucci@hotmail.com>
2025-04-13 20:21:29 -0300

    Ultima atualização movimentação de peças de xadrez - Nível 3

--- xadrez.c
+++ xadrez.c
@@ -1,58 +1,72 @@
 #include <stdio.h>
 
-int main () {
-    // Mover a Torre 5 casas para a direita
-
-    printf("Torre\n");
-    for (int t = 0; t < 5; t++) {
-        printf("Direita\n"); //Iprime a direção do movimento e quantas casas        
+void moverTorre(int casas) {
+    if (casas > 0) {
+        printf("Direita\n");
+        moverTorre(casas - 1);
     }
 
+}
+
+void moverBispo(int casas) {
+    if (casas > 0) {
+        for (int i = 0; i < 1; i++) { //Vertical cima
+            printf("Cima -");
+            for (int j = 0; j < 1; j++) { // Horizontal direita
+                printf(" Direita\n");
+            }            
+        }
+        moverBispo(casas - 1); //Chamada recursiva
+    }
+}
+
+void moverRainha (int casas) {
+    if (casas > 0) {
+        printf("Esquerda\n");
+        moverRainha(casas -1);
+    }
+}
+
+int main () {
+
+    // Mover a Torre 5 casas para a direita
+    
+    printf("Torre\n");
+    moverTorre(5);
     printf("\n");
 
 
     // Mover o Bispo 5 casas na diagonal para cima e a direita
 
-    int b = 0;
-
     printf("Bispo\n");
-    while (b < 5)
-    {
-        printf("Cima -");
-        printf(" Direita\n");
-        b++;
-    }
-
+    moverBispo(5);
     printf("\n");
 
-    // Mover a Rainha 8 casas para a esquerda
 
-    int r = 0;
+    // // Mover a Rainha 8 casas para a esquerda
 
     printf("Rainha\n");
-    do {
-        printf("Esquerda\n");
-        r++;
-    } while (r < 8);
-
+    moverRainha(8);
     printf("\n");
 
-    // Mover o Cavalo duas casas para baixo e uma casa para a esquerda
 
-    int a = 0;
+
+    // // Mover o Cavalo duas casas para baixo e uma casa para a esquerda
+
+        // printf("Cavalo\n");
+    // for (int c = 0, a = 0; c < 1 || a < 2; c++, a++)
+    // {
+    // printf("Baixo\n");          
+    // }
+    // printf("Esquerda");
+    // printf("\n");
 
     printf("Cavalo\n");
-    for (int c = 0; c < 1; c++)
-    {
-        while (a < 2)
-        {
-            printf("baixo\n");
-            a++;
-        }
-        printf("Esquerda");
-        printf("\n");
+
+    for (int i = 0; i < 3; i++) {
+        (i < 2) ? printf("Cima\n") : printf("Direita\n");
     }
-    
+
     
     return 0;
 }
